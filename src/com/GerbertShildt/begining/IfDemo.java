72545d93b135c8d09d78176af75be9680ada13ef
thevoynov <thevoynov@yandex.ru>
2021-03-10 21:54:04 +0300

    Continuation of JAVA learning

--- src/com/GerbertShildt/begining/IfDemo.java
+++ src/com/GerbertShildt/begining/IfDemo.java
@@ -9,26 +9,22 @@ class IfDemo {
 
         if (a < b)
             System.out.println("a меньше b");
-        //  Следующая строка никогда не будет выполнена
-        if (a == b)
+        if (a == b) //  Следующая строка никогда не будет выполнена
             System.out.println("Вы не должны увидеть этот текст");
-
         System.out.println();
 
         c = a - b;  // переменная "c" содержит значения -1
-
         System.out.println("c содержит -1");
 
         if (c >= 0)
             System.out.println("c - не отрицательное число");
         if (c < 0)
             System.out.println("c - отрицательное число");
-
         System.out.println();
 
         c = b - a;  //  переменная "c" теперь содержит значения 1
-
         System.out.println("c содержит 1");
+
         if (c >= 0)
             System.out.println("c - не отрицательное число");
         if(c < 0)
