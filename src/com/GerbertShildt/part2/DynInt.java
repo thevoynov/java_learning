f1cce20b8f22071252affdaddef6e4dd3390f8a5
thevoynov <thevoynov@yandex.ru>
2021-03-11 23:19:08 +0300

    Continuation of JAVA learning

---
+++ src/com/GerbertShildt/part2/DynInt.java
@@ -0,0 +1,11 @@
+package com.GerbertShildt.part2;
+
+class DynInt {
+    public static void main(String args[]) {
+        double radius = 4, height = 5;
+        //Переменная volume инициализируется динамически
+        //во время выполнения программы
+        double volume = 3.1416 * radius * radius * height;
+        System.out.println("Объём: " + volume);
+    }
+}
---
+++ src/com/GerbertShildt/part2/StrDemo.java
@@ -0,0 +1,14 @@
+package com.GerbertShildt.part2;
+/*
+    Демонстрация управляющих последовательностей в
+    символьных строках
+ */
+class StrDemo {
+    public static void main(String args[]) {
+        System.out.println("Первая строка \nВторая строка");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+
+    }
+
+}
