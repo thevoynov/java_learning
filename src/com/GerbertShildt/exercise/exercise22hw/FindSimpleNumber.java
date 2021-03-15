ff4ef2ba40ad89b4c0c8528d7b45cb5af8f2a0ae
Voynov <thevoynov@yandex.ru>
2021-03-15 15:43:14 +0300

    I added javadoc

--- src/com/GerbertShildt/exercise/exercise22hw/FindSimpleNumber.java
+++ src/com/GerbertShildt/exercise/exercise22hw/FindSimpleNumber.java
@@ -2,12 +2,32 @@ package com.GerbertShildt.exercise.exercise22hw;
 
 class FindSimpleNumber {
     public static void main(String args[]) {
-        byte b = 2;
-        for (b = 2; b == 100; b++) {
-            b = 2;
-            b++;
-            b %= (byte) b;
-            System.out.println("Найденные простые числа в диапазоне от 2 до 100: " + b);
+        byte n = 2;
+        byte x = 0;
+        byte results = 0;
+        byte nr = 0;
+        byte nr_2 = 0;
+        boolean b = false;
+
+        for(n = 2; n < 100; n++) {
+            for(x = 2; x < n; x++) {
+                System.out.println("n = " + n);
+                System.out.println("x = " + x);
+                results = (byte) (n % x);
+                System.out.println("results " + results);
+                if (results == 0) {
+                    b = true;
+                    nr = n;
+                }
+                else {
+                    b = false;
+
+                }
+                System.out.println("b : " + b);
+                if(results > 0 & !b & nr != n) {
+                    System.out.println("Найдено простое число: " + n);
+                }
+            }
         }
     }
 }
