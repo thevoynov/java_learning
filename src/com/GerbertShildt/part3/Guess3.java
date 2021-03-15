294a24433b8dabced315efcf03478aeee2028780
thevoynov <thevoynov@yandex.ru>
2021-03-15 23:48:47 +0300

    Continuation of JAVA learning

---
+++ src/com/GerbertShildt/part3/Guess3.java
@@ -0,0 +1,21 @@
+package com.GerbertShildt.part3;
+
+class Guess3 {
+    public static void main(String args[])
+        throws java.io.IOException {
+        char ch, answer = 'K';
+        System.out.println("Задумана буква из диапазона A-Z.");
+        System.out.println("Попытайтесь её угадать: ");
+        ch = (char) System.in.read();   //  чтение символа с клавиатуры
+        if(ch == answer)
+            System.out.println("Правильно!");
+        else {
+            System.out.println("Извините, нужная буква находится ");
+            if(ch < answer)
+                System.out.println("Ближе к конце алфавита");
+            else
+                System.out.println("Ближе к началу алфавита");
+        }
+
+    }
+}
