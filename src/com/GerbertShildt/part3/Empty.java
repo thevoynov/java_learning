912430a00e54367be09f63cb585891bfdd87f2e3
thevoynov <thevoynov@yandex.ru>
2021-03-16 23:05:40 +0300

    Continuation of JAVA learning

---
+++ src/com/GerbertShildt/part3/Empty.java
@@ -0,0 +1,12 @@
+package com.GerbertShildt.part3;
+
+public class Empty {
+    public static void main(String args[]) {
+        int i;
+
+        for(i = 0; i < 10; ) {          //  отсутсвует итерационное выражение
+            System.out.println("Проход #" + i);
+            i++;   //   инкрементирование переменной цикла
+        }
+    }
+}
