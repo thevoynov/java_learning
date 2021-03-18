4b287d2c7d4031c955dd42875c00722f4c79dac2
thevoynov <thevoynov@yandex.ru>
2021-03-18 20:14:25 +0300

    Ending of part 3

---
+++ src/com/GerbertShildt/exercise/exercise33hw/SymbolCount.java
@@ -0,0 +1,36 @@
+package com.GerbertShildt.exercise.exercise33hw;
+
+public class SymbolCount {
+    public static void main(String args[])
+        throws java.io.IOException {
+        char choice = '0';
+        char ignore = '0';
+        char choice_sp = '0';
+        boolean b = true;
+        int t = 0;
+        int t_sp = 0;
+        do {
+            System.out.println("Угадайте задуманный символ " +
+                                "и введите его с клавиатуры:");
+            choice = (char) System.in.read();
+            if(choice == ' ') {
+                t_sp++;
+            }
+            if(choice == '.') {
+                b = false;
+                System.out.println("ДААА! Вы угадали.");
+                System.out.println("Количество раз, сколько " +
+                                    "был нажат пробел " + t_sp);
+            }
+            else {
+                t++;
+                System.out.println(t + "-ая попытка");
+                System.out.println("Нет! Попробуйте ещё.");
+            }
+
+            do {
+                ignore = (char) System.in.read();
+            }   while (ignore != '\n');
+        } while (b);
+    }
+}
---
+++ src/com/GerbertShildt/part3/ContToLabel.java
@@ -0,0 +1,16 @@
+package com.GerbertShildt.part3;
+
+class ContToLabel {
+    public static void main(String args[]) {
+outerloop:
+        for(int i = 1; i < 10; i++) {
+            System.out.print("\nВнешний цикл: проход "
+                                + i + ", внутренний цикл ");
+            for(int j = 1; j < 10; j++) {
+                if(j == 5) continue outerloop;  //  продолжение внешнего
+                                                //  цикла
+                System.out.print(j);
+            }
+        }
+    }
+}
