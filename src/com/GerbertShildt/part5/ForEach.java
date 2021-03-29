0ec8c6e6b3f1e492d35b0bd753ef6993af359bdb
thevoynov <thevoynov@yandex.ru>
2021-03-29 10:03:13 +0300

    learning

---
+++ src/com/GerbertShildt/part5/ForEach.java
@@ -0,0 +1,18 @@
+package com.GerbertShildt.part5;
+
+public class ForEach {
+    public static void main(String args[]) {
+        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        //  использование цикла типа for-each
+        //  суммирование отображение значений
+
+        for(int x: nums) {
+            System.out.println("Значение: " + x);
+            sum += x;
+            if(x == 5) break;
+        }
+        System.out.println("Сумма этих значений: " + sum);
+    }
+}
---
+++ src/com/GerbertShildt/part5/StringDemo.java
@@ -0,0 +1,16 @@
+package com.GerbertShildt.part5;
+
+public class StringDemo {
+    public static void main(String args[]) {
+        //  Различные способы объявления строк
+        String str1 = new String("В Java строки - это объекты");
+        String str2 = "Их можно создавать разными способами";
+        String str3 = new String(str2);
+        String str4 = str1;
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+    }
+}
