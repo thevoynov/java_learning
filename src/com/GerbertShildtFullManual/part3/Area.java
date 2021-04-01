fc9fc0de7b1163c361bde7ac88fc583566ad43e1
Voynov <thevoynov@yandex.ru>
2021-04-01 17:09:24 +0300

    Part5 Arrays, String

--- src/com/GerbertShildtFullManual/part3/Area.java
+++ src/com/GerbertShildtFullManual/part3/Area.java
@@ -1,19 +1,14 @@
 package com.GerbertShildtFullManual.part3;
 // import java.io.IOException;
+import java.util.Scanner;
 public class Area {
     public static void main(String args[])
         throws java.io.IOException {
         double pi, r, a;
-        System.out.println("Введите радиус окружности: ");
-        r = System.in.read();
-        pi = Math.PI;
-        a = pi * Math.pow(r, 2);
-
-        System.out.println("Площадь круга равно " + a);
 
         r = 10.8;
-        pi = 3.1416;
-        a = pi * r * r;
+        pi = Math.PI;
+        a = pi * Math.pow(r, 2);
 
         System.out.println("Площадь круга 2 равно " + a);
     }
