5b53ac19a45b2611515c5cf3c1512c3415f9edbf
Voynov <thevoynov@yandex.ru>
2021-04-05 17:55:15 +0300

    Part5 Arrays, String

---
+++ src/com/GerbertShildtFullManual/part3/CharDemo.java
@@ -0,0 +1,11 @@
+package com.GerbertShildtFullManual.part3;
+
+class CharDemo {
+    public static void main(String args[]) {
+        char ch1, ch2;
+        ch1 = 88;
+        ch2 = 'Y';
+        System.out.print("ch 1 и ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
