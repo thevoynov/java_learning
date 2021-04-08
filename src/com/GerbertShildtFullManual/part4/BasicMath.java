953b949158104edda28060b31a736e8cc2263a28
Voynov <thevoynov@yandex.ru>
2021-04-08 16:59:34 +0300

    Part5 Arrays, String

---
+++ src/com/GerbertShildtFullManual/part4/BasicMath.java
@@ -0,0 +1,32 @@
+package com.GerbertShildtFullManual.part4;
+
+class BasicMath {
+    public static void main(String args[]) {
+        //  Арифметические операции надо
+        //  целочисленным значениями
+        System.out.println("Целочисленная арифметика");
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+
+        //  Арифметеческ операции над значениями типа double
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - da;
+        double de = -dd;
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de = " + de);
+
+    }
+}
