d546ec6c3208cb45923138cfd7fccc0d7eb059a3
Voynov <thevoynov@yandex.ru>
2021-06-07 10:59:58 +0300

    part5

---
+++ src/com/GerbertShildtFullManual/part5/DoWhileV2.java
@@ -0,0 +1,11 @@
+package com.GerbertShildtFullManual.part5;
+
+class DoWhileV2 {
+    public static void main(String args[]) {
+        int n = 10;
+
+        do {
+            System.out.println("такт " + n);
+        }   while (--n > 0);
+    }
+}
