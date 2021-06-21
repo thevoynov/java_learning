00cac416dd9f162295900b531aa50270dcc2d461
Voynov <thevoynov@yandex.ru>
2021-06-21 16:45:03 +0300

    Part7 was done

---
+++ src/com/GerbertShildtFullManual/part7/VarArgs.java
@@ -0,0 +1,24 @@
+package com.GerbertShildtFullManual.part7;
+
+//  продемонстрировать применение аргументов
+//  переменной длины
+
+class VarArgs {
+    //  теперь метод vaTest() объявляется с аргументами
+    //  переменной длинны
+    static  void vaTest(int ... v) {
+        System.out.print("Количество аргументов: " + v.length
+                + " Содержимое: ");
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        //  обратите внимание на возможные способы вызова
+        //  метода vaTest() с аргументами переменной длины
+        vaTest(10);
+        vaTest(1, 2, 3);
+        vaTest();
+    }
+}
