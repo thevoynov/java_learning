227f403efcb4366cf09f27e6c1a151c3a5cd73d9
Voynov <thevoynov@yandex.ru>
2021-06-21 12:59:51 +0300

    Part7

---
+++ src/com/GerbertShildtFullManual/part7/StringDemo.java
@@ -0,0 +1,9 @@
+package com.GerbertShildtFullManual.part7;
+
+class StringDemo {
+    public static void main(String args[]) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = "Первая строка";
+    }
+}
