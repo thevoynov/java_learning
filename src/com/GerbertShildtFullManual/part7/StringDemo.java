59f173e5365b52f1e213ff41d97b40db738ebdf1
Voynov <thevoynov@yandex.ru>
2021-06-21 13:07:26 +0300

    Part7

--- src/com/GerbertShildtFullManual/part7/StringDemo.java
+++ src/com/GerbertShildtFullManual/part7/StringDemo.java
@@ -4,6 +4,18 @@ class StringDemo {
     public static void main(String args[]) {
         String strOb1 = "Первая строка";
         String strOb2 = "Вторая строка";
-        String strOb3 = "Первая строка";
+        String strOb3 = strOb1;
+
+        System.out.println("Длинна строки strob1: " + strOb1.length());
+        System.out.println("Символ по индексу 3 в строке strOb1: " + strOb1.charAt(3));
+
+        if(strOb1.equals(strOb2))
+            System.out.println("strOb1 == strOb2");
+        else
+            System.out.println("strOb1 != strOb2");
+        if(strOb1.equals(strOb3))
+            System.out.println("strOb1 == strOb3");
+        else
+            System.out.println("strOb1 != strOb3");
     }
 }
