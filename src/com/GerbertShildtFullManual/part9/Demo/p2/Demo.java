2b61fe956ef77b56dfff544edea9ed0c5289edf4
Voynov <thevoynov@yandex.ru>
2021-06-25 16:40:50 +0300

    Part9

--- src/com/GerbertShildtFullManual/part9/Demo/p2/Demo.java
+++ src/com/GerbertShildtFullManual/part9/Demo/p2/Demo.java
@@ -28,6 +28,6 @@ class OtherPackage {
 public class Demo {
     public static void main(String args[]) {
         Protection2 ob1 = new Protection2();
-       OtherPackage ob2 = new OtherPackage();
+        OtherPackage ob2 = new OtherPackage();
     }
 }
