b2b0dd98642ad6964ded89fb23764c991f5f9ea0
Voynov <thevoynov@yandex.ru>
2021-07-02 15:37:40 +0300

    Alishev Thread pool

---
+++ src/com/Alishev/AdvancedJava/LearnProducer/Test.java
@@ -0,0 +1,21 @@
+package com.Alishev.AdvancedJava.LearnProducer;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Test {
+    BlockingQueue<Integer> queue
+            = new ArrayBlockingQueue<>(10);
+    public static  void main(String[] args) {
+
+    }
+
+    private static void produce() {
+        Random random = new Random(100);
+
+        while (true) {
+
+        }
+    }
+}
