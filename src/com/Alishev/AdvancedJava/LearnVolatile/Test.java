de63b545d8e9670c6a1f92dc6732ae46a4562ec5
Voynov <thevoynov@yandex.ru>
2021-07-02 15:12:59 +0300

    Alishev Thread pool

---
+++ src/com/Alishev/AdvancedJava/LearnVolatile/Test.java
@@ -0,0 +1,33 @@
+package com.Alishev.AdvancedJava.LearnVolatile;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String args[]) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+    private volatile boolean running = true;
+
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void shutdown() {
+        this.running = false;
+    }
+}
