afa327462157e3eea76328e988cd4d25287b30ee
Voynov <thevoynov@yandex.ru>
2021-07-06 11:47:56 +0300

    Alishev

---
+++ src/com/Alishev/Beginning/Lesson25/Dog.java
@@ -0,0 +1,10 @@
+package com.Alishev.Beginning.Lesson25;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("Im barking");
+    }
+    public void eat() {
+        System.out.println("Dog's eating");
+    }
+}
