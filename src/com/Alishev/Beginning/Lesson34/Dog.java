50616ec0f9eaf777b70162a62efdce61c7400d57
Voynov <thevoynov@yandex.ru>
2021-07-06 17:23:59 +0300

    Alishev

---
+++ src/com/Alishev/Beginning/Lesson34/Dog.java
@@ -0,0 +1,5 @@
+package com.Alishev.Beginning.Lesson34;
+
+public class Dog extends Animal {
+
+}
---
+++ src/com/Alishev/Beginning/Lesson34/Test.java
@@ -0,0 +1,27 @@
+package com.Alishev.Beginning.Lesson34;
+import java.util.ArrayList;
+import java.util.List;
+public class Test {
+    public static void main(String[] args) {
+        List<Animal> listOfAnimal = new ArrayList<>();
+        listOfAnimal.add(new Animal(1));
+        listOfAnimal.add(new Animal(2));
+
+        List<Dog> listOfDogs = new ArrayList<>();
+        listOfDogs.add(new Dog());
+        listOfDogs.add(new Dog());
+
+        test(listOfAnimal);
+        test(listOfDogs);
+    }
+
+    private static void test(Animal animal) {
+
+    }
+
+    private static void test(List<? extends Animal> list) {
+        for (Animal animal : list) {
+            System.out.println(animal);
+        }
+     }
+}
